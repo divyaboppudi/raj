dd6b20e0df485ec1c97340f13a838cc7f67b314c
divyaboppudi <157440232+divyaboppudi@users.noreply.github.com>
2024-01-31 23:58:27 -0500

    Update Source3.cpp

--- DSA_CLASS1_CODE2/Source3.cpp
+++ DSA_CLASS1_CODE2/Source3.cpp
@@ -1,13 +1,14 @@
-
-#include <iostream>
-using namespace std;
-int main()
+// Title: Iterating Through an Integer Array
+// Description: A C++ program that declares and initializes an integer array and then iterates through its elements using a range-based for loop.
+#include <iostream>     //Include the Input/Output Stream Library for input and output operations.
+using namespace std;    //Declare that the program will use the std namespace, which contains standard C++ functions and objects.
+int main()              //The main function, where the program execution begins.
 {
-    int mynumbers[5] = { 10, 20, 30, 40, 50 };
-    for (int i : mynumbers)
+    int mynumbers[5] = { 10, 20, 30, 40, 50 };  //Declare and initialize an integer array named 'mynumbers' with five elements.
+    for (int i : mynumbers)                     //Use a range-based for loop to iterate through each element of the 'mynumbers' array.
     {
-        cout << i << "\n";
+        cout << i << "\n";                      //Print the current element followed by a newline character.
     }
 
-    return 0;
+    return 0;                                   //The main function returns 0, indicating successful execution of the program.
 }
