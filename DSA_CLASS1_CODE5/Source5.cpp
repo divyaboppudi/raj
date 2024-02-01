3936be6f682ebf0e294d8e24542d8fc8a0160b15
divyaboppudi <157440232+divyaboppudi@users.noreply.github.com>
2024-02-01 00:07:51 -0500

    Update Source5.cpp

--- DSA_CLASS1_CODE5/Source5.cpp
+++ DSA_CLASS1_CODE5/Source5.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-using namespace std;
-#include <iostream>
-using namespace std;
-int main()
+// Title: Size of Integer Array and Element
+// Description: A C++ program that declares and initializes an integer array, prints the size of the array, and then prints the size of an individual integer element.
+#include <iostream>   // Include the Input/Output Stream Library for input and output operations.
+using namespace std;  //Declare that the program will use the std namespace, which contains standard C++ functions and objects.
+int main()            //The main function, where the program execution begins.
 {
 
-	int mynumbers[5] = { 10,20,30,40,50 };
-	cout << sizeof(mynumbers) << endl;
-	cout << sizeof(int) << endl;
+	int mynumbers[5] = { 10,20,30,40,50 };    //Declare and initialize an integer array named 'mynumbers' with five elements.
+	cout << sizeof(mynumbers) << endl;        //Print the size (in bytes) of the 'mynumbers' array.
+	cout << sizeof(int) << endl;              //Print the size (in bytes) of an individual integer element.
+	return 0;                                 //The main function returns 0, indicating successful execution of the program.
 }
