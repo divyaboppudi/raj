7fd46c601d6eae5e5efe16695c1039c550ea6b5b
divyaboppudi <157440232+divyaboppudi@users.noreply.github.com>
2024-02-01 00:25:05 -0500

    Update Source.cpp

--- DSA_CLASS_cODE3/Source.cpp
+++ DSA_CLASS_cODE3/Source.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-using namespace std;
+// Title: Simple Hello World Program
+// Description: A basic C++ program that prints the "Hello World" message to the console.
+#include <iostream>          // Include the Input/Output Stream Library for input and output operations.
+using namespace std;         //Declare that the program will use the std namespace, which contains standard C++ functions and objects.
 
-int main() {
+int main()                    //The main function, where the program execution begins
+{
 
-	cout << "Hello World";
+	cout << "Hello World";  //Print the "Hello World" message to the console.
+	return 0;               //The main function returns 0, indicating successful execution of the program.
 }
