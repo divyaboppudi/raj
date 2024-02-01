fc8ac04444719718082e8404d2776dead1e118ea
divyaboppudi <157440232+divyaboppudi@users.noreply.github.com>
2024-02-01 00:12:22 -0500

    Update Source6.cpp

--- DSA_CLASS1_CODE6/Source6.cpp
+++ DSA_CLASS1_CODE6/Source6.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
-using namespace std;
+// Title: Iterating Through an Array of Car Brands
+// Description: A C++ program that declares and initializes an array of car brands, calculates the array size, and then iterates through the elements using a for loop.
 
-int main()
+#include <iostream>           //Include the Input/Output Stream Library for input and output operations.
+using namespace std;          //Declare that the program will use the std namespace, which contains standard C++ functions and objects.
+
+int main()                    //The main function, where the program execution begins.
 {
-
-
-    string cars[5] = { "volvo", "toyato", "ford", "tsla", "honda" };
-    int n = sizeof(cars) / sizeof(string);
-    for (int i{}; i < n; i++)
+    string cars[5] = { "volvo", "toyato", "ford", "tsla", "honda" }; //Declare and initialize a string array named 'cars' with five car brands.
+    int n = sizeof(cars) / sizeof(string);     //Calculate the number of elements in the 'cars' array.
+    for (int i{}; i < n; i++)                  //Use a for loop to iterate through each element of the 'cars' array.
     {
-        cout << cars[i] << "\n";
+        cout << cars[i] << "\n";               //Print each car brand followed by a newline character.
     }
-
+return 0;                                      //The main function returns 0, indicating successful execution of the program.
 }
